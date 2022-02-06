a020bb276e7b58d6c9bf0e3baf734c1214caecb1
simon <neocoin@gmail.com>
2022-02-06 22:35:39 +0900

    🔧 level1, level2 application

--- scripts/setup-mac.rb
+++ scripts/setup-mac.rb
@@ -10,30 +10,42 @@ require 'rouge'
 TS = Time.now
 module SetupMac
   module Helper
-    ENV_APPS = %w[direnv nodenv rbenv pyenv pyenv-virtualenv]
-    CUI_APPS = %w[coreutils git ripgrep fzf gh jq bat rb features tldr starship git-flow-avh gitmoji git-lfs openjdk colordiff kubernetes-cli kube-score k9s]
-    GUI_INSTALL_SCRIPT = <<~BASH
-      brew install --cask google-chrome      # 크롬
-      brew install --cask jetbrains-toolbox  # jetbrains 툴 설치 및 업데이트 관리
-      brew install --cask docker             # docker desktop
-      brew install --cask firefox            # 파폭
-      brew install --cask notion             # Notion 데스크탑 
-      brew install --cask iterm2             # 가장 많이 쓰이는 터미널 소프트웨어
-      brew install --cask sourcetree         # git gui 도구
-      brew install --cask github             # github gui 도구
-      brew install --cask dash               # 개발자 문서 도구
-      brew install --cask visual-studio-code # Visual Studio Code
-      brew install --cask microsoft-office   # MS Office
-      brew install --cask bloomrpc           # grpc client
-      brew install --cask itsycal            # calendar
-      brew install --cask wireshark          # packet monitor
-      brew install --cask altair-graphql-client # rest client
-      brew install --cask insomnia           # rest client
-      brew install --cask monitorcontrol     # 외장 모니터 밝기 조절, 볼륨 조정
-      brew install homebrew/cask-fonts/font-d2coding
-      brew install homebrew/cask-fonts/font-jetbrains-mono
-      brew install homebrew/cask-fonts/font-fira-code-nerd-font
-    BASH
+    ENV_APPS = Set[*%w[direnv nodenv rbenv pyenv pyenv-virtualenv]].to_a
+    CUI_APPS = Set[*%w[coreutils git ripgrep fzf gh jq bat rb features tldr starship git-flow-avh gitmoji git-lfs openjdk colordiff kubernetes-cli kube-score k9s]].to_a
+
+    LEVEL1_CASK_APPS = {
+      'google-chrome'         => '크롬',
+      'jetbrains-toolbox'     => 'jetbrains 툴 설치 및 업데이트 관리',
+      'docker'                => 'docker desktop',
+      'firefox'               => '파폭',
+      'notion'                => 'Notion 데스크탑',
+      'iterm2'                => '가장 많이 쓰이는 터미널 소프트웨어',
+      'sourcetree'            => 'git gui 도구',
+      'dash'                  => '개발자 문서 도구',
+      'visual-studio-code'    => 'Visual Studio Code',
+      'microsoft-office'      => 'MS Office',
+      'bloomrpc'              => 'grpc client',
+      'altair-graphql-client' => 'rest client',
+      'insomnia'              => 'rest client',
+    }
+
+    LEVEL2_CASK_APPS = {
+      'itsycal'         => 'calendar',
+      'wireshark'       => 'packet monitor',
+      'monitorcontrol'  => '외장 모니터 밝기 조절, 볼륨 조정',
+      'notion-enhanced' => 'Notion 데스크탑 커스텀 버전',
+      'google-drive'    => 'Google Drive',
+      'github'          => 'github gui 도구',
+      'alt-tab'         => 'Windows-like alt-tab',
+      'keepingyouawake' => 'Tool to prevent the system from going into sleep mode',
+      'rectangle'       => 'Move and resize windows using keyboard shortcuts or snap areas',
+    }
+
+    FONTS = Set[
+      'homebrew/cask-fonts/font-d2coding',
+      'homebrew/cask-fonts/font-jetbrains-mono',
+      'homebrew/cask-fonts/font-fira-code-nerd-font'
+    ].to_a
 
     MAS_INSTALL_SCRIPT = <<~BASH
       mas install 803453959  # slack
@@ -43,23 +55,23 @@ module SetupMac
     BASH
 
     MAS_APPS = MAS_INSTALL_SCRIPT.lines.filter { |i| /mas install/ =~ i }.map(&:split).map { |i| i[2] }
-    GUI_APPS = GUI_INSTALL_SCRIPT.lines.filter { |i| /--cask/ =~ i }.map(&:split).map { |i| i[3] }
-    GUI_APPS_EXT = %w[google-drive]
+    # GUI_APPS = GUI_INSTALL_SCRIPT.lines.filter { |i| /--cask/ =~ i }.map(&:split).map { |i| i[3] }
+    GUI_APPS = LEVEL1_CASK_APPS.keys + LEVEL2_CASK_APPS.keys
 
     def run(cmd)
       system(cmd.tap { |o| puts Rainbow(o).yellow })
     end
 
     def mas_signin?
-      !`! mas account > /dev/null && echo 'no account'`.include?('no account')
+      !`! mas account > /dev/null && echo ' no account '`.include?(' no account ')
     end
 
     def brew?
-      `command -v brew > /dev/null && echo 'exists'`.include?('exists')
+      `command -v brew > /dev/null && echo ' exists '`.include?(' exists ')
     end
 
     def ts
-      TS;
+      TS
     end
 
     def rename_file_if_exists(target)
@@ -70,7 +82,7 @@ module SetupMac
     end
 
     def install_home(filename)
-      source = Pathname.new(`brew --prefix setup-mac`.strip) / 'share' / filename
+      source = Pathname.new(`brew --prefix setup-mac`.strip) / ' share ' / filename
       target = Pathname.new(Dir.home) / filename
       rename_file_if_exists(target)
       FileUtils.cp source, target
@@ -86,25 +98,30 @@ module SetupMac
 
     def remain_apps
       {
-        env: {
-          template: "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
-          remain: ENV_APPS - brew_apps,
+        env:   {
+          template:         "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain:           ENV_APPS - brew_apps,
           install_template: "brew install %s",
         },
-        cui: {
-          template: "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
-          remain: CUI_APPS - brew_apps,
+        cui:   {
+          template:         "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain:           CUI_APPS - brew_apps,
           install_template: "brew install %s",
         },
-        gui: {
-          template: "미설치된 개발 GUI 앱이 발견되었습니다. %s 설치하시겠습니까? (brew 외 방법으로 설치된 경우는 해당 앱만 실패합니다.) [Y/n]",
-          remain: GUI_APPS - brew_apps,
+        gui:   {
+          template:         "미설치된 개발 GUI 앱이 발견되었습니다. %s 설치하시겠습니까? (brew 외 방법으로 설치된 경우는 해당 앱만 실패합니다.) [Y/n]",
+          remain:           GUI_APPS - brew_apps,
           install_template: "brew install --cask %s",
         },
-        mas: {
-          template: "미설치된 맥 앱스토어 앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
-          remain: (MAS_APPS - mas_apps),
+        mas:   {
+          template:         "미설치된 맥 앱스토어 앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain:           (MAS_APPS - mas_apps),
           install_template: "mas install %s",
+        },
+        fonts: {
+          template:         "미설치된 폰트가 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain:           FONTS.reject { |i| brew_apps.include?(i.split('/').last) },
+          install_template: "brew install %s",
         },
       }.reject { |_, v| v[:remain].empty? }
     end
@@ -118,10 +135,10 @@ module SetupMac
     desc "env", "디버깅용으로, 환경을 출력 한다."
 
     def env
-      ruby_env = {
-        RUBY_VERSION: RUBY_VERSION,
+      ruby_env   = {
+        RUBY_VERSION:  RUBY_VERSION,
         RUBY_PLATFORM: RUBY_PLATFORM,
-        RUBY_ENGINE: RUBY_ENGINE }
+        RUBY_ENGINE:   RUBY_ENGINE }
       key_length = ENV.keys.max_by { |i| i.size }.size
       ENV.to_h.merge(ruby_env)
          .to_a.sort_by { |i| i.to_s }.each { |k, v| puts "#{Rainbow(k.to_s.rjust(key_length + 2)).green}: #{v}" }
@@ -138,13 +155,13 @@ module SetupMac
 
       run "brew install -q zinit"
       run "mkdir -p ~/.zinit"
-      run "ln -sf #{Pathname.new(`brew --prefix zinit`.strip) / 'libexec' / 'zinit'}  ~/.zinit/bin"
+      # run "ln -sf #{Pathname.new(`brew --prefix zinit`.strip) / ' libexec ' / ' zinit '}  ~/.zinit/bin"
 
       installed = `brew list`.split
-      installs = (ENV_APPS + CUI_APPS - installed)
+      installs  = (ENV_APPS + CUI_APPS - installed)
       installs.each { |i| run "brew install #{i}" }
       run "brew install --HEAD goenv"
-      installs.include?('gitmoji') && run("NODE_TLS_REJECT_UNAUTHORIZED=0 gitmoji -l > /dev/null 2>&1")
+      installs.include?(' gitmoji ') && run("NODE_TLS_REJECT_UNAUTHORIZED=0 gitmoji -l > /dev/null 2>&1")
 
       %w[.zshrc .vimrc].each { |i| install_home(i) }
 
@@ -152,22 +169,24 @@ module SetupMac
       doctor "cui"
 
       puts Rainbow("새로운 터미널을 열어주세요.").yellow
-      run("setup-mac hint")
     end
 
     desc "install_gui_apps", "업무에 필요한 기본 mac gui 프로그램들을 설치한다."
 
     def install_gui_apps
-      GUI_INSTALL_SCRIPT.lines.map(&:strip).each { |i| run i }
+      # GUI_INSTALL_SCRIPT.lines.map(&:strip).each { |i| run i }
+      LEVEL1_CASK_APPS.keys.each { |i| run "brew install --cask %s" % i }
+      LEVEL2_CASK_APPS.keys.each { |i| run "brew install --cask %s" % i }
+      FONTS.each { |i| run "brew install %s" % i }
       doctor "gui"
     end
 
     desc "install_mas_apps", "업무에 필요한 기본 mac gui 프로그램들을 설치한다."
 
     def install_mas_apps
-      run 'brew install -q mas'
+      run ' brew install - q mas '
       unless mas_signin?
-        run 'mas open 803453959'
+        run ' mas open 803453959 '
         puts "먼저 Mac App Store 에 로그인해주세요."
         exit 1
       end
@@ -177,6 +196,7 @@ module SetupMac
     end
 
     desc "hint", "문제에 대한 힌트"
+
     def hint
       puts <<~EOF
         # 자동완성 권한 관련 메세지가 나오면 다음을 실행한다.
@@ -188,11 +208,11 @@ module SetupMac
     desc "readme", "도구에 대한 설명"
 
     def readme
-      readme = Pathname.new(`brew --prefix setup-mac`.strip) / 'share' / 'setup-mac.readme.md'
+      readme   = Pathname.new(`brew --prefix setup-mac`.strip) / ' share ' / ' setup - mac.readme.md '
       contents = readme.read
 
       formatter = Rouge::Formatters::Terminal256.new
-      lexer = Rouge::Lexers::Markdown.new
+      lexer     = Rouge::Lexers::Markdown.new
       puts formatter.format(lexer.lex(contents))
     end
 
@@ -200,11 +220,11 @@ module SetupMac
 
     def doctor(categories = nil)
       puts Rainbow("환경을 확인합니다.").yellow if categories.nil?
-      categories ||= %w[env gui cui mas]
+      categories ||= remain_apps.keys.map(&:to_s)
 
       remain_apps.select { |k, _| categories.include?(k.to_s) }.each do |_, v|
         apps = v[:remain]
-        puts v[:template] % (apps * ',')
+        puts v[:template] % (apps * ', ')
         apps.each { |i| run(v[:install_template] % i.to_s) } if STDIN.gets.downcase.strip.then { |o| ['y', ''].include? o }
       end
     end
