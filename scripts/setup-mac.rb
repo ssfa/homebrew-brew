1e5f0d48075bde1df8a7e3f6e3bb7eaed06309cd
Sangmin Ryu <neocoin@gmail.com>
2024-04-18 01:51:21 +0900

    ✨ monaspace font

--- scripts/setup-mac.rb
+++ scripts/setup-mac.rb
@@ -15,37 +15,34 @@ module SetupMac
 
     CASK_APPS = {
       # uncategory
-      'docker'                => 'docker desktop',
-      'notion'                => 'Notion 데스크탑',
-      'notion-enhanced'       => 'Notion 데스크탑 커스텀 버전',
-      'iterm2'                => '가장 많이 쓰이는 터미널 소프트웨어',
-      'dash'                  => '개발자 문서 도구',
-      'bloomrpc'              => 'grpc client',
+      'docker' => 'docker desktop',
+      'notion' => 'Notion 데스크탑',
+      'iterm2' => '가장 많이 쓰이는 터미널 소프트웨어',
+      'dash' => '개발자 문서 도구',
+      'bloomrpc' => 'grpc client',
       'altair-graphql-client' => 'Grpahql client',
-      'insomnia'              => 'rest client',
-      'figma'                 => 'Collaborative team software',
-      'wireshark'       => 'packet monitor',
-      'google-drive'    => 'Google Drive',
-      '1password'       => '1password',
+      'insomnia' => 'rest client',
+      'google-drive' => 'Google Drive',
+      '1password' => '1password',
       # browser
       'google-chrome' => '크롬',
-      'firefox'       => '파폭',
+      'firefox' => '파폭',
       # editor
-      'jetbrains-toolbox'  => 'jetbrains 툴 설치 및 업데이트 관리',
-      'Visual-studio-code' => 'Visual Studio Code',
-      'microsoft-office'   => 'MS Office',
+      'jetbrains-toolbox' => 'jetbrains 툴 설치 및 업데이트 관리',
+      'visual-studio-code' => 'Visual Studio Code',
+      'microsoft-office' => 'MS Office',
       # windows util
-      'alt-tab'         => 'Windows-like alt-tab',
-      'hiddenbar'       => 'Utility to hide menu bar items',
-      'amethyst'        => 'Automatic tiling window manager similar to xmonad',
-      'rectangle'       => 'Move and resize windows using keyboard shortcuts or snap areas',
+      'alt-tab' => 'Windows-like alt-tab',
+      'hiddenbar' => 'Utility to hide menu bar items',
+      'amethyst' => 'Automatic tiling window manager similar to xmonad',
+      'rectangle' => 'Move and resize windows using keyboard shortcuts or snap areas',
       # bar util
-      'itsycal'         => 'calendar',
+      'itsycal' => 'calendar',
       'keepingyouawake' => 'Tool to prevent the system from going into sleep mode',
-      'monitorcontrol'        => '외장 모니터 밝기 조절, 볼륨 조정',
+      'monitorcontrol' => '외장 모니터 밝기 조절, 볼륨 조정',
       # chat
-      'discord'         => 'Voice and text chat software',
-      'mattermost'      => 'Open-source, self-hosted Slack-alternative',
+      'discord' => 'Voice and text chat software',
+      'mattermost' => 'Open-source, self-hosted Slack-alternative',
     }
 
     CASK_VERSION_APPS = {
@@ -55,7 +52,9 @@ module SetupMac
     FONTS = Set[
       'homebrew/cask-fonts/font-d2coding',
       'homebrew/cask-fonts/font-jetbrains-mono',
-      'homebrew/cask-fonts/font-fira-code-nerd-font'
+      'homebrew/cask-fonts/font-fira-code-nerd-font',
+      'homebrew/cask-fonts/font-monaspace',
+      'homebrew/cask-fonts/font-monaspace-nerd-font',
     ].to_a
 
     GUI_APPS = CASK_APPS.keys
@@ -100,24 +99,24 @@ module SetupMac
 
     def remain_apps
       {
-        env:   {
-          template:         "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
-          remain:           ENV_APPS - brew_apps,
+        env: {
+          template: "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain: ENV_APPS - brew_apps,
           install_template: "brew install %s",
         },
-        cui:   {
-          template:         "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
-          remain:           CUI_APPS - brew_apps,
+        cui: {
+          template: "미설치된 개발 환경앱이 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain: CUI_APPS - brew_apps,
           install_template: "brew install %s",
         },
-        gui:   {
-          template:         "미설치된 개발 GUI 앱이 발견되었습니다. %s 설치하시겠습니까? (brew 외 방법으로 설치된 경우는 해당 앱만 실패합니다.) [Y/n]",
-          remain:           GUI_APPS - brew_apps,
+        gui: {
+          template: "미설치된 개발 GUI 앱이 발견되었습니다. %s 설치하시겠습니까? (brew 외 방법으로 설치된 경우는 해당 앱만 실패합니다.) [Y/n]",
+          remain: GUI_APPS - brew_apps,
           install_template: "brew install --cask %s",
         },
         fonts: {
-          template:         "미설치된 폰트가 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
-          remain:           FONTS.reject { |i| brew_apps.include?(i.split('/').last) },
+          template: "미설치된 폰트가 발견되었습니다. %s 설치하시겠습니까? [Y/n]",
+          remain: FONTS.reject { |i| brew_apps.include?(i.split('/').last) },
           install_template: "brew install %s",
         },
       }.reject { |_, v| v[:remain].empty? }
@@ -132,10 +131,10 @@ module SetupMac
     desc "env", "디버깅용으로, 환경을 출력 한다."
 
     def env
-      ruby_env   = {
-        RUBY_VERSION:  RUBY_VERSION,
+      ruby_env = {
+        RUBY_VERSION: RUBY_VERSION,
         RUBY_PLATFORM: RUBY_PLATFORM,
-        RUBY_ENGINE:   RUBY_ENGINE }
+        RUBY_ENGINE: RUBY_ENGINE }
       key_length = ENV.keys.max_by { |i| i.size }.size
       ENV.to_h.merge(ruby_env)
          .to_a.sort_by { |i| i.to_s }.each { |k, v| puts "#{Rainbow(k.to_s.rjust(key_length + 2)).green}: #{v}" }
@@ -155,7 +154,7 @@ module SetupMac
       # run "ln -sf #{Pathname.new(`brew --prefix zinit`.strip) / ' libexec ' / ' zinit '}  ~/.zinit/bin"
 
       installed = `brew list`.split
-      installs  = (ENV_APPS + CUI_APPS - installed)
+      installs = (ENV_APPS + CUI_APPS - installed)
       installs.each { |i| run "brew install #{i}" }
       run "brew install --HEAD goenv"
       installs.include?(' gitmoji ') && run("NODE_TLS_REJECT_UNAUTHORIZED=0 gitmoji -l > /dev/null 2>&1")
@@ -190,11 +189,11 @@ module SetupMac
     desc "readme", "도구에 대한 설명"
 
     def readme
-      readme   = Pathname.new(`brew --prefix setup-mac`.strip) / ' share ' / ' setup - mac.readme.md '
+      readme = Pathname.new(`brew --prefix setup-mac`.strip) / ' share ' / ' setup - mac.readme.md '
       contents = readme.read
 
       formatter = Rouge::Formatters::Terminal256.new
-      lexer     = Rouge::Lexers::Markdown.new
+      lexer = Rouge::Lexers::Markdown.new
       puts formatter.format(lexer.lex(contents))
     end
 
